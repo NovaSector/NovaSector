cbd72277afd05f30a0b6f499a8162412fe3663ea
Giz <13398309+vinylspiders@users.noreply.github.com>
2023-12-27 00:22:36 -0500

    Revert "Fix Opfor jank"
    This reverts commit 6af2fd71d85e6be58890f27f4f32bf92cfcb00fb.

--- tgui/packages/tgui/interfaces/OpposingForcePanel.jsx
+++ tgui/packages/tgui/interfaces/OpposingForcePanel.jsx
@@ -203,6 +203,7 @@ export const OpposingForceTab = (props) => {
       <Stack.Item>
         <Section title="Backstory">
           <TextArea
+            disabled={!can_edit}
             height="100px"
             value={backstory}
             placeholder="Provide a description of why you want to do bad things. Include specifics such as what lead upto the events that made you want to do bad things, think of it as though you were your character, react appropriately. If you don't have any ideas, check the #player-shared-opfors channel for some. (2000 char limit)"
@@ -421,6 +422,7 @@ export const OpposingForceObjectives = (props) => {
                   <Stack.Item>
                     <TextArea
                       fluid
+                      disabled={!can_edit}
                       height="85px"
                       value={selectedObjective.description}
                       onChange={(e, value) =>
@@ -445,6 +447,7 @@ export const OpposingForceObjectives = (props) => {
                   </Stack.Item>
                   <Stack.Item>
                     <TextArea
+                      disabled={!can_edit}
                       height="85px"
                       value={selectedObjective.justification}
                       onChange={(e, value) =>
