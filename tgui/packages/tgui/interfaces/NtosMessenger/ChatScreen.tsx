26b24530c4829faadc5962e5d28c185fa8ecff64
Giz <13398309+vinylspiders@users.noreply.github.com>
2023-12-27 21:31:50 -0500

    Makes direct messenger messages self-clear

--- tgui/packages/tgui/interfaces/NtosMessenger/ChatScreen.tsx
+++ tgui/packages/tgui/interfaces/NtosMessenger/ChatScreen.tsx
@@ -303,6 +303,7 @@ export class ChatScreen extends Component<ChatScreenProps, ChatScreenState> {
                   <Input
                     placeholder={`Send message to ${recipient.name}...`}
                     fluid
+                    selfClear
                     autoFocus
                     width="100%"
                     value={message}
