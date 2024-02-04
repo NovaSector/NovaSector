acddd0059cdc6f6addab4b994c2f9e0a74a91ea6
nikothedude <simon.prouty@gmail.com>
2024-02-04 13:47:58 -0500

    i need to push this shit already

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/nova/antag_optin.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/nova/antag_optin.tsx
@@ -4,8 +4,10 @@ import { FeatureChoiced, FeatureDropdownInput } from '../../base';
 export const antag_opt_in_status_pref: FeatureChoiced = {
   name: 'Be Antagonist Target',
   description:
-    'Enabling any non-ghost antags \
-  (revenant, abductor contractor, etc.) will force your opt-in to be, \
-  at minimum, "Temporarily Inconvenience".',
+    'This is for objective targetting and OOC consent.\
+    By extension, picking "Round Remove" will allow you to be round removed in applicable situations, even by non-antagonists. \
+    Enabling any non-ghost antags \
+    (revenant, abductor contractor, etc.) will force your opt-in to be, \
+    at minimum, "Temporarily Inconvenience".',
   component: FeatureDropdownInput,
 };
