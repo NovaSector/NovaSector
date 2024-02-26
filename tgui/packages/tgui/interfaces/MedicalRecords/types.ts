b6049161e013f2a3015aa5a010a3f4c64007c8fe
_0Steven <42909981+00-Steven@users.noreply.github.com>
2024-02-26 18:35:20 +0100

    Fix custom job titles displaying with question mark icons in medical/security records.

--- tgui/packages/tgui/interfaces/MedicalRecords/types.ts
+++ tgui/packages/tgui/interfaces/MedicalRecords/types.ts
@@ -26,10 +26,14 @@ export type MedicalRecord = {
   quirk_notes: string;
   rank: string;
   species: string;
+<<<<<<< HEAD
   // NOVA EDIT START - RP Records
   past_general_records: string;
   past_medical_records: string;
   // NOVA EDIT END
+=======
+  trim: string;
+>>>>>>> 13128fcfbba (Fix custom job titles displaying with question mark icons in medical/security records. (#81637))
 };
 
 export type MedicalNote = {
