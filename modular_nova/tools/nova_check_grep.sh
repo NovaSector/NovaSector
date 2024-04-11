2e3aeeb7e197897dc2494d730f4d2b7dd4134a6f
Paxilmaniac <paxilmaniac@gmail.com>
2024-04-11 19:20:52 -0400

    makes greps check the lethalstation folder

--- modular_nova/tools/nova_check_grep.sh
+++ modular_nova/tools/nova_check_grep.sh
@@ -10,3 +10,7 @@ echo -e "${BLUE}Re-running grep checks, but looking in modular_nova...${NC}"
 
 # Run the linters again, but modular Nova Sector code.
 sed "s|code/\*\*/\*\.dm|modular_nova/\*\*/\*\.dm|g" <tools/ci/check_grep.sh | bash
+
+# Run the linters again again but for our special funny code - THIS AND THE TWO LINES BELOW ARE LETHALSTATION EDITS BY THE WAY
+echo -e "${BLUE}Re-running grep checks, but looking in modular_np_lethal...${NC}"
+sed "s|code/\*\*/\*\.dm|modular_np_lethal/\*\*/\*\.dm|g" <tools/ci/check_grep.sh | bash
