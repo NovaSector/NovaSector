705b19871515cb5336e9da4f7f04971974455545
David Sinclair <Ephemeralis@users.noreply.github.com>
2024-06-12 07:46:26 +1000

    Add genemodded quirk

---
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/nova/genemodded.tsx
@@ -0,0 +1,10 @@
+// THIS IS A NOVA SECTOR UI FILE
+import {
+  FeatureChoiced,
+} from '../../base';
+import { FeatureDropdownInput } from '../../dropdowns';
+
+export const genemodded_dna: FeatureChoiced = {
+  name: 'Mutation',
+  component: FeatureDropdownInput,
+};
