337659f672e1581244e95a2d10a406dc9e17b03d
OrbisAnima <orbisanima@gmail.com>
2025-02-17 22:36:42 -0300

    Initial Commit

---
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/nova/sounds.tsx
@@ -0,0 +1,8 @@
+import { CheckboxInput, FeatureToggle } from '../../base';
+
+export const sound_eating: FeatureToggle = {
+  name: 'Enable eating and drinking sounds',
+  category: 'SOUND',
+  description: 'When enabled, hear eating and drinking sounds when appropriate.',
+  component: CheckboxInput,
+};
