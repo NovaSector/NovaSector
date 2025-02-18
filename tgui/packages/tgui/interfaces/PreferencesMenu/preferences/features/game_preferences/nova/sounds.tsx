d9080d7419efd0847222da33c0744dc2132d44b6
OrbisAnima <orbisanima@gmail.com>
2025-02-18 11:29:16 -0300

    Fucking Lint

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/nova/sounds.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/nova/sounds.tsx
@@ -3,6 +3,7 @@ import { CheckboxInput, FeatureToggle } from '../../base';
 export const sound_eating: FeatureToggle = {
   name: 'Enable eating and drinking sounds',
   category: 'SOUND',
-  description: 'When enabled, hear eating and drinking sounds when appropriate.',
+  description:
+    'When enabled, hear eating and drinking sounds when appropriate.',
   component: CheckboxInput,
 };
