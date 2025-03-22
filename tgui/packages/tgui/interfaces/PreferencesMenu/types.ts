592e48cd8133ab8700f6bdedc7d8e871a9fa7817
Dani Glore <fantasticdragons@gmail.com>
2025-03-22 14:19:55 -0400

    Cleaning up

--- tgui/packages/tgui/interfaces/PreferencesMenu/types.ts
+++ tgui/packages/tgui/interfaces/PreferencesMenu/types.ts
@@ -204,9 +204,9 @@ export type PreferencesMenuData = {
       gender: Gender;
       joblessrole: JoblessRole;
       species: string;
-      loadout_lists: LoadoutList; // BUBBER EDIT: Multiple loadout presets: ORIGINAL: loadout_list: LoadoutList;
+      loadout_lists: LoadoutList; // NOVA EDIT: Multiple loadout presets: ORIGINAL: loadout_list: LoadoutList;
       job_clothes: BooleanLike;
-      loadout_index: string; // BUBBER EDIT ADDITION: Multiple loadout presets
+      loadout_index: string; // NOVA EDIT ADDITION: Multiple loadout presets
     };
 
     randomization: Record<string, RandomSetting>;
