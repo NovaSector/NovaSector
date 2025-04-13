ed9767b13ec97af36771e44037a06b0e413e7b78
Aylong <69762909+AyIong@users.noreply.github.com>
2025-04-13 11:48:17 +0300

    Fix UI identations (#90568)
    ## About The Pull Request
    Well... I glimpsed all 344 files where `Stack.Item` is used (on sidebar
    search), and fixed the indents wherever gap broke them, I'm sure I
    missed something, but the most noticeable problems should be resolved
    
    I slightly improved a couple of interfaces out of boredom,
    video/screenshots below (there aren't many (2))
    
    <details> <summary> Guh </summary>
    
    
    https://github.com/user-attachments/assets/0004a3d6-e141-4044-a3f7-0002d901523e
    
    
    ![image](https://github.com/user-attachments/assets/194886fd-546f-4571-89f8-d9009f4debd5)
    
    </details>
    
    ## Why It's Good For The Game
    I don't even know, like anyone even cares about the extra indentations
    I should take a good break from such boring activities.....

--- tgui/packages/tgui/interfaces/AntagInfoTraitor.tsx
+++ tgui/packages/tgui/interfaces/AntagInfoTraitor.tsx
@@ -92,7 +92,7 @@ const EmployerSection = (props) => {
               <BlockQuote>{allies}</BlockQuote>
             </Stack.Item>
             <Stack.Divider />
-            <Stack.Item>
+            <Stack.Item mb={1}>
               <span style={goalstyle}>
                 Employer thoughts:
                 <br />
@@ -125,7 +125,7 @@ const UplinkSection = (props) => {
               )}
             </Stack.Item>
             <Stack.Divider />
-            <Stack.Item mt="1%">
+            <Stack.Item align="center">
               <BlockQuote>{uplink_unlock_info}</BlockQuote>
             </Stack.Item>
           </>
