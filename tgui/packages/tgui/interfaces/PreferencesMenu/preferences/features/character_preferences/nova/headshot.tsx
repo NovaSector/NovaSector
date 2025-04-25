f8106a53677bcb9bb292c2b771852e6631038ec2
Dani Glore <fantasticdragons@gmail.com>
2025-04-24 20:26:52 -0400

    Add reference to ImgBox and Byond Members files in headshot info

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/nova/headshot.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/nova/headshot.tsx
@@ -3,9 +3,9 @@ import { Feature, FeatureShortTextInput } from '../../base';
 
 const description =
   'Requires a link ending with .png, .jpeg, or .jpg, starting with \
-  https://, and hosted on Gyazo or Discord. Renders the image underneath \
-  your character preview in the examine more window. \
-  Image larger than 250x250 will be resized to 250x250. \
+  https://, and hosted on Gyazo, ImgBox, or Byond Members files. \
+  Renders the image underneath your character preview in the examine \
+  more window. Image larger than 250x250 will be resized to 250x250. \
   Aim for 250x250 whenever possible';
 
 export const headshot: Feature<string> = {
