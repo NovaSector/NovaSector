NovaSector/NovaSector

commit 595a882303ba9976f223543e7fc8d152aeb1a0b2

[SEMI-MODULAR] Neuroware & Enhanced Synths (#4503)
## About The Pull Request

Fixes #5389 

This PR aims to add actual functionality to the synth stomach and liver,
as they are currently only re-skinned cybernetic organs.

After tgstation/tgstation#71937 was added, reagents became a lot more
intelligent about when their side effects are allowed to happen. The
benefit of that PR was that we started using flags for biotype,
bodytype, and organtype a lot more adequately and in a standard way
across the codebase. This draft PR builds on that progress by adding
functionality to the synthetic stomach and reagent processor organ so
that they affect how reagents are processed.

## Overview Of Changes

There are some bugfixes and three major changes included in the PR:

### 1. Neuroware Chips & Brain-Based Drug Processing

Drug reagents now process only if the mob has an organic brain, allowing
them to more safely process in synth species. This will prevent any odd
side-effects from happening due to a synth humanoid processing
organic-oriented drugs. This PR also happens to disable
`/datum/reagent/drug/twitch` for synthetic humanoids as well, providing
more balance to the species.

Adds a new collection of "Neuroware chip" items which allows synthetic
humanoids and NIF-equipped human mobs to install "neurocomputing
software". The disks insert special reagent subtypes when used. To
better contextualize these subtypes as programs instead of chemicals,
they have `chemical_flags` set to `REAGENT_INVISIBLE` and are
non-synthesizable. Each neuroware disk manually adds reagents to the
target mob, instead of maintaining them in a reagents container.

Neuroware is detectable by the user via
`/datum/status_effect/neuroware`, which appears on their screen.
Neuroware is also detectable via health analyzers or any other mob
chemical scan, listing the installed programs. You can remove neuroware
reagents from a mob via the "system reset neuroware" or via brain

modular_nova/master_files/code/modules/surgery/surgery.dm
@@ -2,3 +2,30 @@
 	/// Whether this surgery aims to remove or replace the target bodypart, with the goal of being used
 	/// with a bodypart's `can_be_surgically_removed` variable. Defaults to FALSE.
 	var/removes_target_bodypart = FALSE
+	///What organ type is required. Used with organ_to_manipulate.
+	var/requires_organ_type
+	///What organ flags are required. Used with organ_to_manipulate.
+	var/requires_organ_flags
+	///What amount of damage is required on the organ before the surgery can start. Used with organ_to_manipulate.
+	var/requires_organ_damage
+
+// Prevents surgery if the target organ's bodyzone, damage, type, or flags differ from expected.
+/datum/surgery/can_start(mob/user, mob/living/patient)
+	. = ..()
+	if(isnull(organ_to_manipulate))
+		return
+	var/obj/item/organ/target_organ = patient.get_organ_slot(organ_to_manipulate)
+	if(isnull(target_organ))
+		return FALSE
+	// Ensure organ is where it's needed
+	if(target_organ.zone != location)
+		return FALSE
+	// Ensure organ has the required amount of damage
+	if(!isnull(requires_organ_damage) && target_organ.damage < requires_organ_damage)
+		return FALSE
+	// Ensure organ is the required type
+	if(!isnull(requires_organ_type) && !istype(target_organ, requires_organ_type))
+		return FALSE
+	// Ensure organ has required flags
+	if(!isnull(requires_organ_flags) && !(target_organ.organ_flags & requires_organ_flags))
+		return FALSE

commit 54cacdae212986635dae67d21998d49eb2f03eb8

Fix: Missing Head Surgeries (#5667)
## About The Pull Request

This PR fixes head organ surgeries, which had gone missing from the
surgery list due to my own oversight.

I made a small developer oversight in #4503 wherein I added an override
to `/datum/surgery/can_start()`. In this override I utilized the
`location` variable, however during `can_start()` the variable is always
hardcoded to `BODY_ZONE_CHEST`, and isn't really usable at the point I
was trying to use it. This essentially caused surgeries to always assume
they were operating on the chest, and fail to validate the location of
their targeted organs.

## How This Contributes To The Nova Sector Roleplay Experience

The PR fixes a bug introduced by PR #4503 by allowing several surgeries
to appear in the surgery initiator list again.

## Proof of Testing

<details>
<summary>Screenshots/Videos</summary>


![image](https://github.com/user-attachments/assets/c868bf96-1d26-4025-8d45-fb85ca9a406f)

</details>

## Changelog

:cl: A.C.M.O.
fix: Fixed head organ surgeries, which were not appearing in the surgery
list.
/:cl:

modular_nova/master_files/code/modules/surgery/surgery.dm
@@ -18,7 +18,7 @@
 	if(isnull(target_organ))
 		return FALSE
 	// Ensure organ is where it's needed
-	if(target_organ.zone != location)
+	if(!(target_organ.zone in possible_locs))
 		return FALSE
 	// Ensure organ has the required amount of damage
 	if(!isnull(requires_organ_damage) && target_organ.damage < requires_organ_damage)