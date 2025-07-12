648261ff98b44091ee69a7d0f65ee30cd9606f35
SomeRandomOwl <2568378+SomeRandomOwl@users.noreply.github.com>
2025-07-12 19:58:04 -0400

    Linters begone

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/nova/floating_hands.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/nova/floating_hands.tsx
@@ -1,6 +1,6 @@
 import { Feature, FeatureColorInput } from '../../base';
 
 export const floating_items: Feature<string> = {
-    name: 'Hover Effect Color',
-    component: FeatureColorInput,
+  name: 'Hover Effect Color',
+  component: FeatureColorInput,
 };
