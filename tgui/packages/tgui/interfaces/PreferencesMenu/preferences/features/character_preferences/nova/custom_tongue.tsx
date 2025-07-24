f1c9e6f8757ff4767266ba6b7c94326b9220cb83
Chestlet <chestlet@protonmail.com>
2025-07-24 15:34:30 -0700

    makes the tgui panel tell you A-Z only.

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/nova/custom_tongue.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/nova/custom_tongue.tsx
@@ -3,30 +3,30 @@ import { Feature, FeatureShortTextInput } from '../../base';
 
 export const custom_tongue_ask: Feature<string> = {
   name: 'Ask?',
-  description: 'Automated Ask say modifier.',
+  description: 'Automated Ask say modifier. A-Z Only.',
   component: FeatureShortTextInput,
 };
 
 export const custom_tongue_exclaim: Feature<string> = {
   name: 'Exclaim!',
-  description: 'Automated Exclaim say modifier.',
+  description: 'Automated Exclaim say modifier. A-Z Only.',
   component: FeatureShortTextInput,
 };
 
 export const custom_tongue_whisper: Feature<string> = {
   name: 'whisper',
-  description: 'Automated Whisper say modifier.',
+  description: 'Automated Whisper say modifier. A-Z Only.',
   component: FeatureShortTextInput,
 };
 
 export const custom_tongue_yell: Feature<string> = {
   name: 'Yell!!',
-  description: 'Automated Yell say modifier.',
+  description: 'Automated Yell say modifier. A-Z Only.',
   component: FeatureShortTextInput,
 };
 
 export const custom_tongue_say: Feature<string> = {
   name: 'Say.',
-  description: 'Automated Say say modifier.',
+  description: 'Automated Say say modifier. A-Z Only.',
   component: FeatureShortTextInput,
 };
