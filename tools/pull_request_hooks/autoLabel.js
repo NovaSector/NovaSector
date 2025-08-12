d4dfdb6f3eaab34eff4ff46a778d0de7ea631e20
Bloop <13398309+vinylspiders@users.noreply.github.com>
2025-08-12 14:52:58 -0400

    Update autoLabel.js

--- tools/pull_request_hooks/autoLabel.js
+++ tools/pull_request_hooks/autoLabel.js
@@ -33,10 +33,15 @@ const titleKeywordSets = (() => {
  */
 const fileLabelFilepathSets = (() => {
   const map = {};
-  for (const [label, { filepaths, add_only }] of Object.entries(
-    autoLabelConfig.file_labels
-  )) {
-    map[label] = { filepaths: new Set(filepaths), add_only };
+  for (const [
+    label,
+    { filepaths = [], file_extensions = [], add_only },
+  ] of Object.entries(autoLabelConfig.file_labels)) {
+    map[label] = {
+      filepaths: new Set(filepaths),
+      file_extensions: new Set(file_extensions),
+      add_only,
+    };
   }
   return map;
 })();
@@ -126,26 +131,31 @@ async function check_diff_files_for_labels(github, context) {
 
     for (const [
       label,
-      { filepaths = [], file_extensions = [], add_only },
+      { filepaths = new Set(), file_extensions = new Set(), add_only },
     ] of Object.entries(fileLabelFilepathSets)) {
       let found = false;
 
-      // Path-based pattern matching
-      for (const filepath of filepaths) {
-        if ([...changedFiles].some((filename) => filename.includes(filepath))) {
-          found = true;
-          break;
+      // Filepath-based matching
+      for (const filename of changedFiles) {
+        for (const path of filepaths) {
+          if (filename.includes(path)) {
+            found = true;
+            break;
+          }
         }
+        if (found) break;
       }
 
-      if (!found && file_extensions.length) {
-        // File extension-based matching
-        if (
-          [...changedFiles].some((filename) =>
-            file_extensions.some((ext) => filename.endsWith(ext))
-          )
-        ) {
-          found = true;
+      // File extension-based matching
+      if (!found && file_extensions.size) {
+        for (const filename of changedFiles) {
+          for (const ext of file_extensions) {
+            if (filename.endsWith(ext)) {
+              found = true;
+              break;
+            }
+          }
+          if (found) break;
         }
       }
 
