71643ae26d23c7032467b1910cf652a962796c72
Lila S. <sheepwiththemask@gmail.com>
2025-08-31 17:59:46 +0200

    review

--- tgui/packages/tgui/interfaces/PreferencesMenu/antagonists/antagonists/spaceninja.ts
+++ tgui/packages/tgui/interfaces/PreferencesMenu/antagonists/antagonists/spaceninja.ts
@@ -6,10 +6,12 @@ const SpaceNinja: Antagonist = {
   /* NOVA EDIT ADDITION START */
   description: [
     `
-      The upper-echelon of Cybersun operatives practice a sort of augmentation of human flesh in order to
-      achieve a more perfect state of being and follow Postmodern Space Bushido.
+      The Spider Clan are a small and mysterious faction, said to be neutrally aligned
+      yet often carry out the most dangerous contracts available in the sector.
+      Cybersun Industries persues Nanotrasen's technological advances closely, making
+      a name for itself as their most feared competitor. The Spider Clan bushido are
+      happy to oblige in driving that point home...
     `,
-
     `
       Become a conniving space ninja, equipped with a katana, gloves to hack
       into airlocks and APCs, a suit to make you go near-invisible,
