2cb67cb308443d704c756c65aae26d7e8160e408
Bloop <13398309+vinylspiders@users.noreply.github.com>
2025-09-22 19:18:30 -0400

    Update tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/nova/bodytempature_customization.tsx

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/nova/bodytempature_customization.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/nova/bodytempature_customization.tsx
@@ -5,7 +5,7 @@ export const bodytemp: Feature<number> = {
   description: `
     Your body temperature can be set different from the standard,
     with the highest temp of 70 being equivalent to a skrell or as low as -20 with being equivalent to a plasmaman.
-    Note that your safety is not guaranteed within the extremes, be mindful of your existance.
+    Note that your safety is not guaranteed within the extremes, be mindful of your existence.
   `,
   component: FeatureNumberInput,
 };
