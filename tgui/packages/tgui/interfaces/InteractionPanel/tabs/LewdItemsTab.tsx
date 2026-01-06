1bd1f2501dffc724f026cda4ec2344fea4f4662f
Adeheuer <117639764+Adeheuer@users.noreply.github.com>
2026-01-06 14:35:11 +0300

    Update tgui/packages/tgui/interfaces/InteractionPanel/tabs/LewdItemsTab.tsx
    Co-authored-by: Bloop <13398309+vinylspiders@users.noreply.github.com>

--- tgui/packages/tgui/interfaces/InteractionPanel/tabs/LewdItemsTab.tsx
+++ tgui/packages/tgui/interfaces/InteractionPanel/tabs/LewdItemsTab.tsx
@@ -82,8 +82,6 @@ export const LewdItemsTab = ({searchText}: LewdItemsTabProps) => {
                 );
             })}
             </Stack>
-        ) : (
-            ''
         )}
     </Stack>
   );
