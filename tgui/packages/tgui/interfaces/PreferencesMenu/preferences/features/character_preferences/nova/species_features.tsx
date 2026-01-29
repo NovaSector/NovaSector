a04fb1b4499ad2afe2710df6d9bf2b19a554a337
BugWasabi <143133070+SunriseOverYourHead@users.noreply.github.com>
2026-01-29 10:42:46 -0300

    quadarms Char Preference commit

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/nova/species_features.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/nova/species_features.tsx
@@ -795,3 +795,22 @@ export const pod_hair_emissive: Feature<boolean> = {
   description: 'Emissive parts glow in the dark.',
   component: CheckboxInput,
 };
+
+export const fourarms_toggle: FeatureToggle = {
+  name: 'Quad Arms',
+  component: CheckboxInput,
+};
+
+export const feature_fourarms: Feature<string> = {
+  name: 'Quad Arms Selection',
+  component: (
+    props: FeatureValueProps<string, string, FeatureChoicedServerData>,
+  ) => {
+    return <FeatureDropdownInput buttons {...props} />;
+  },
+};
+
+export const fourarms_color: Feature<string[]> = {
+  name: 'Quad Arms Color',
+  component: FeatureTriColorInput,
+};
