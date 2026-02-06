826ea4a5cfed10a8d620d4b4394b31ae1baf0562
John F. Kennedy <54908920+Adelphon@users.noreply.github.com>
2026-02-06 13:47:33 -0500

    Description Update

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/nova/antag_optin.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/nova/antag_optin.tsx
@@ -5,10 +5,10 @@ import { FeatureDropdownInput } from '../../dropdowns';
 export const antag_opt_in_status_pref: FeatureChoiced = {
   name: 'Be Antagonist Target',
   description:
-    'This is for objective targetting and OOC consent.\
+    'This is for objective targetting, YOU CAN STILL BE ATTACKED WITH PROPER ESCALATION.\
     By extension, picking "Round Remove" will allow you to be round removed in applicable situations, even by non-antagonists. \
     Enabling any non-ghost antags \
     (revenant, abductor contractor, etc.) will force your opt-in to be, \
-    at minimum, "Temporarily Inconvenience".',
+    at minimum, "Yes".',
   component: FeatureDropdownInput,
 };
