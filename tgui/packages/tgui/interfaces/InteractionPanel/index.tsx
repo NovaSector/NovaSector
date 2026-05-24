2cd4e8a366e9d770fe981a96991155798d0cd006
pepe <john@john.com>
2026-05-24 16:14:43 +0200

    fix conflict

--- tgui/packages/tgui/interfaces/InteractionPanel/index.tsx
+++ tgui/packages/tgui/interfaces/InteractionPanel/index.tsx
@@ -17,7 +17,7 @@ type Interaction = {
   erp_interaction: BooleanLike;
   has_erp_interaction: BooleanLike;
   simulated_genitals?: SimulatedGenital[];
-}
+};
 
 export function InteractionPanel() {
   const { act, data } = useBackend<Interaction>();
@@ -32,53 +32,9 @@ export function InteractionPanel() {
   return (
     <Window width={500} height={600} title={`Interact - ${self}`}>
       <Window.Content scrollable>
-          {!!erp_interaction && !!has_erp_interaction && (
-            <Section>
-              <Stack vertical fill>
-                  <Stack.Item grow>
-                    <InfoSection />
-                  </Stack.Item>
-              </Stack>
-
-              <LabeledList>
-                <Button.Checkbox
-                  checked={use_subtler}
-                  onClick={() =>
-                    act('toggle_subtler', {
-                      use_subtler: !use_subtler,
-                    })
-                  }
-                  tooltip="Untick to make lewd interactions visible to all mobs in range able to perceive them."
-                >
-                  Use Subtler
-                </Button.Checkbox>
-              </LabeledList>
-
-            </Section>
-          )}
-
-          {!!simulated_genitals?.length && (
-            <Section title="Simulated Genitals (cyborg)">
-              <LabeledList>
-                {simulated_genitals.map((slot) => (
-                  <Button.Checkbox
-                    key={slot.name}
-                    checked={!!slot.active}
-                    onClick={() =>
-                      act('toggle_genital_active', {
-                        genital: slot.name,
-                      })
-                    }
-                    tooltip={`Toggle whether lewd interactions treat you as having a ${slot.name} slot.`}
-                  >
-                    {slot.name}
-                  </Button.Checkbox>
-                ))}
-              </LabeledList>
-            </Section>
-          )}
-
-          <Stack>
+        {!!erp_interaction && !!has_erp_interaction && (
+          <Section>
+            <Stack vertical fill>
               <Stack.Item grow>
                 <InfoSection />
               </Stack.Item>
@@ -96,6 +52,27 @@ export function InteractionPanel() {
               >
                 Use Subtler
               </Button.Checkbox>
+            </LabeledList>
+          </Section>
+        )}
+
+        {!!simulated_genitals?.length && (
+          <Section title="Simulated Genitals (cyborg)">
+            <LabeledList>
+              {simulated_genitals.map((slot) => (
+                <Button.Checkbox
+                  key={slot.name}
+                  checked={!!slot.active}
+                  onClick={() =>
+                    act('toggle_genital_active', {
+                      genital: slot.name,
+                    })
+                  }
+                  tooltip={`Toggle whether lewd interactions treat you as having a ${slot.name} slot.`}
+                >
+                  {slot.name}
+                </Button.Checkbox>
+              ))}
             </LabeledList>
           </Section>
         )}
