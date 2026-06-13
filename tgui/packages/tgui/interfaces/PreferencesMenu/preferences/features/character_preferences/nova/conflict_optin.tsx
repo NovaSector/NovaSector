ac83ee3f52e2712a03cc81cf8af31f127fef95f1
ImogenOC <heckoftoday@gmail.com>
2026-06-13 15:40:07 +0200

    description update. this is way too long i think

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/nova/conflict_optin.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/nova/conflict_optin.tsx
@@ -5,7 +5,11 @@ import { FeatureDropdownInput } from '../../dropdowns';
 export const conflict_opt_in_status_pref: FeatureChoiced = {
   name: 'Be a Part of the Conflict',
   description:
-    'This is for OOC consent to engage in conflict. Pick carefully, as this will affect your policy experience and guide your decisions. \
-    You should review the "Conflict Opt-In System" documentation on the wiki to see the current definitions.',
+    'This is your Conflict Opt-In selection, which dictates how you are expected to behave when exposed to antagonism or criminal behavior directed at you or your department. It also dictates how other Crew should handle you when going about their criminal business. \
+    These selections are primarily dictated by the Server Policy on the Wiki or Discord references, but loose expectations for each selection are: \
+    Passive crew should behave non-aggressively or only in direct self defense, and should not be targeted with antagonistic action. They are expected to not interfere with criminal activity or ongoing antagonists, even if it disadvantages them or their department. \
+    Involved crew accept confrontation that is unlikely to result in their death, but that may otherwise disadvantage them. They can be targeted for antagonistic action, as can later opts. \
+    Vulnerable crew accept conflict that may result in their death. \
+    Expendable crew may be used as a target for any conflict or antagonistic action which results in their destruction, total disfigurement, transformation, or otherwise technical removal from the round such as by deep-spacing.',
   component: FeatureDropdownInput,
 };
