97e31027c91bd7fde006c2847291c7c059a91053
ImogenOC <heckoftoday@gmail.com>
2026-06-13 15:56:38 +0200

    updates feature description

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/nova/conflict_optin.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/nova/conflict_optin.tsx
@@ -7,9 +7,9 @@ export const conflict_opt_in_status_pref: FeatureChoiced = {
   description:
     'This is your Conflict Opt-In selection, which dictates how you are expected to behave when exposed to antagonism or criminal behavior directed at you or your department. It also dictates how other Crew should handle you when going about their criminal business. \
     These selections are primarily dictated by the Server Policy on the Wiki or Discord references, but loose expectations for each selection are: \
-    Passive crew should behave non-aggressively or only in direct self defense, and should not be targeted with antagonistic action. They are expected to not interfere with criminal activity or ongoing antagonists, even if it disadvantages them or their department. \
-    Involved crew accept confrontation that is unlikely to result in their death, but that may otherwise disadvantage them. They can be targeted for antagonistic action, as can later opts. \
-    Vulnerable crew accept conflict that may result in their death. \
-    Expendable crew may be used as a target for any conflict or antagonistic action which results in their destruction, total disfigurement, transformation, or otherwise technical removal from the round such as by deep-spacing.',
+    Passive crew exist as set pieces, excluded from being the focus of major round events or antagonism wherever possible. \
+    Involved crew are accepting of confrontation and conflict within the focus and boundaries of roleplaying and story building. \
+    Vulnerable crew accept all situations, particularly mechanics and violence, while maintaining protection from Round Removal.  \
+    Expendable crew do not care if they live or die. Being gibbed is funny to them and they accept any and all dire consequences which may occur.',
   component: FeatureDropdownInput,
 };
