e76f464a66afe49c7add2b4376c4f087079530ff
pepe <john@john.com>
2026-06-15 17:20:00 +0200

    add tsx

---
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/nova/psionics.tsx
@@ -0,0 +1,8 @@
+// THIS IS A NOVA SECTOR UI FILE
+import type { FeatureChoiced } from '../../base';
+import { FeatureDropdownInput } from '../../dropdowns';
+
+export const psionic_rank: FeatureChoiced = {
+  name: 'Psionic Rank',
+  component: FeatureDropdownInput,
+};
