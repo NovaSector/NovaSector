NovaSector/NovaSector

commit 209b1d7c83e431a2cd2579a3e7f3b0b39756f570

Slime People Revive Fixes (#7490)
## About The Pull Request

This pull request addresses some bugs related to Slime People/Hybrids
upon revival or death.
- [x] Fix #7413 , Slime people turning into Human when revived and
dropping human limbs.
- [x] Fix Slime people/hybrids getting quirk items when revived (such as
gloves, blindfolds, canes, etc..)
- [x] Fix Plasma being consumed if the revival fails for whatever
reason.
- [x] Ensure if Slime people have implants they get dropped as well.
- [x] Fix Slime people sometimes spawning slime organs when dying.
(Slime Organs are not needed when they die, because when they get
revived they get those organs back).

This PR compliments #7508

### As this PR effects slime people, and does some workarounds such as
the organ spilling part, test-merging would be wise to ensure no new
issues come with it.


## How This Contributes To The Nova Sector Roleplay Experience

Most of those are bugs,
As for the organ spilling part, slime players when dead have no use to
their organs as with the current implantation of slime players, upon
revival they will get a new body with new organs.

## Proof of Testing
For the Video, we are using two clients, one is the slime the other is
the attacker.
- The slime has the blind perk which spawns a blindfold.
- For Testing purposes we did
`new_body.set_blood_volume(BLOOD_VOLUME_SAFE + 600)`
- and `if(!do_after(user, 1 SECONDS, src))` for the revival time with
plasma.
- in the last revival we DNR as our slime character and try to revive
them to showcase the plasma was not consumed.



https://github.com/user-attachments/assets/22bccabd-569a-434c-aa6c-0ff2320198e1



## Changelog
:cl: Richard Blonski
add: Player Slimes now will drop their implants upon death.
fix: Player Slimes now get revived with their character preferences/as
actual slimes.
fix: Player Slimes dropping human limbs upon revival.
fix: Player Slimes revival consuming plasma when it fails. 
fix: Player Slimes dropping organs upon death.

modular_nova/modules/customization/modules/mob/living/carbon/human/species/roundstartslime.dm
@@ -186,11 +186,19 @@
 	throw_range = 9 //Oh! That's a baseball!
 	throw_speed = 0.5
 	resistance_flags = INDESTRUCTIBLE | FIRE_PROOF | LAVA_PROOF | UNACIDABLE | ACID_PROOF | FREEZE_PROOF
+	/// This tracks a new body that is created when a slime core is killed, and is used to safely trasnfer quirks and prefrences to this body and than summon it upon revival.
+	var/mob/living/carbon/human/new_body
 
 /obj/item/organ/brain/slime/Initialize(mapload, mob/living/carbon/organ_owner, list/examine_list)
 	. = ..()
 	AddComponent(/datum/component/bubble_icon_override, "slime", BUBBLE_ICON_PRIORITY_ORGAN)
 	colorize()
+
+// Handle the slime core being destroyed, and if it has a new body, delete it as well.
+/obj/item/organ/brain/slime/Destroy(force)
+	if(new_body)
+		QDEL_NULL(new_body)
+	return ..()
 
 /obj/item/organ/brain/slime/examine()
 	. = ..()
@@ -264,11 +272,24 @@
 	core_ejected = TRUE
 	victim.visible_message(span_warning("[victim]'s body completely dissolves, collapsing outwards!"), span_notice("Your body completely dissolves, collapsing outwards!"), span_notice("You hear liquid splattering."))
 	var/atom/death_loc = victim.drop_location()
-	victim.unequip_everything()
-	if(victim.get_organ_slot(ORGAN_SLOT_BRAIN) == src)
-		Remove(victim)
+
+	// Create a new body in nullspace, and transfer their prefrences and quirks into it.
+	new_body = new(null)
+	victim.client?.prefs.safe_transfer_prefs_to(new_body)
+	victim.transfer_quirk_datums(new_body)
+
+	// Drop the Brain/Core, and implants to the floor.
+	for(var/obj/item/organ/organs in victim)
+		if(istype(organs, /obj/item/organ/brain) || istype(organs, /obj/item/implant))
+			Remove(organs, special = TRUE)
 	if(death_loc)
 		forceMove(death_loc)
+		// Cleans up spilled organs - When a mob is attacked, it has a chance to spill all its organs on the ground upon death, for slime people we do not need their organs as they regain them when they get revived.
+		for(var/obj/item/organ/spilled_organ in death_loc)
+			if(istype(spilled_organ, /obj/item/organ/brain) || istype(spilled_organ, /obj/item/implant))
+				continue
+			else
+				qdel(spilled_organ)
 	src.wash(CLEAN_WASH)
 	new death_melt_type(death_loc, victim.dir)
 
@@ -278,7 +299,7 @@
 	if(gps_active) // adding the gps signal if they have activated the ability
 		AddComponent(/datum/component/gps, "[victim]'s Core")
 
-	qdel(victim)
+	qdel(victim) // Remove the body.
 	UnregisterSignal(victim, COMSIG_LIVING_DEATH)
 
 /**
@@ -302,24 +323,24 @@
 	brainmob?.notify_revival("You are being revived!", sound = null, source = src) // no sound since it's a whopping 60 second wait time after this
 	if(!do_after(user, 60 SECONDS, src))
 		to_chat(user, span_warning("You failed to pour the contents of [item] onto [src]!"))
-		return TRUE
+		return FALSE
 
 	user.visible_message(
 		span_notice("[user] pours the contents of [item] onto [src], causing it to form a proper cytoplasm and outer membrane."),
 		span_notice("You pour the contents of [item] onto [src], causing it to form a proper cytoplasm and outer membrane.")
 	)
-	item.reagents.clear_reagents() //removes the whole shit
 	if(isnull(brainmob))
 		user.balloon_alert(user, "brain is not a viable candidate for repair!")
-		return TRUE
-
+		return FALSE
 	brainmob.grab_ghost()
 	if(isnull(brainmob.stored_dna))
 		user.balloon_alert(user, "brain does not contain any dna!")
-		return TRUE
+		return FALSE
 	if(isnull(brainmob.client))
 		user.balloon_alert(user, "brain does not contain a mind!")
-		return TRUE
+		return FALSE
+
+	item.reagents.clear_reagents() // Consume the Plasma.
 	regenerate()
 	return TRUE
 
@@ -330,30 +351,31 @@
 		gps_active = FALSE
 		qdel(GetComponent(/datum/component/gps))
 
-	var/mob/living/carbon/human/new_body = new /mob/living/carbon/human(src.loc)
+	// Retreive the new body we created from nullspace.
+	new_body.forceMove(src.drop_location())
 
-	brainmob.client?.prefs?.safe_transfer_prefs_to(new_body)
+	// Ensure they appear fully nude when revived, since slimes don't regrow clothes.
 	new_body.underwear = "Nude"
 	new_body.bra = "Nude"
-	new_body.undershirt = "Nude" //Which undershirt the player wants
-	new_body.socks = "Nude" //Which socks the player wants
-	brainmob.stored_dna.copy_dna(new_body.dna, transfer_flags = COPY_DNA_SE|COPY_DNA_SPECIES)
-	new_body.dna.features[FEATURE_MUTANT_COLOR] = new_body.dna.features[FEATURE_MUTANT_COLOR]
-	new_body.dna.update_uf_block(FEATURE_MUTANT_COLOR)
-	new_body.real_name = new_body.dna.real_name
-	new_body.name = new_body.dna.real_name
-	new_body.updateappearance(mutcolor_update=1)
-	new_body.domutcheck()
-	new_body.forceMove(get_turf(src))
+	new_body.undershirt = "Nude"
+	new_body.socks = "Nude"
+
+	// Handle Blood
 	new_body.set_blood_volume(BLOOD_VOLUME_SAFE + 60)
-	SSquirks.AssignQuirks(new_body, brainmob.client)
-	src.replace_into(new_body)
-	for(var/obj/item/bodypart/bodypart as anything in new_body.bodyparts)
-		if(!istype(bodypart, /obj/item/bodypart/chest))
-			bodypart.drop_limb()
+
+	// Remove non-chest limbs.
+	for(var/obj/item/bodypart/part in new_body.bodyparts)
+		if(part.body_zone == BODY_ZONE_CHEST)
 			continue
+		part.drop_limb(TRUE)
+
+	// Move the brain/core into the new body
+	src.replace_into(new_body)
+
+	// Notify the player that their body has been rebuilt
 	new_body.visible_message(span_warning("[new_body]'s torso \"forms\" from [new_body.p_their()] core, yet to form the rest."))
 	to_chat(owner, span_purple("Your torso fully forms out of your core, yet to form the rest."))
+	new_body = null // Clear the new_body variable to avoid dangling references
 	return TRUE
 
 // HEALING SECTION

commit 3ba675981eb5c87adeb98a4aef07ccc001304b56

Fixes issues caused by #7490 - Items being deleted, or not dropping. - Brain/Core teleporting to nullspace. - After the 2nd revival, preferences not transferring to the new body.

modular_nova/modules/customization/modules/mob/living/carbon/human/species/roundstartslime.dm
@@ -188,6 +188,8 @@
 	resistance_flags = INDESTRUCTIBLE | FIRE_PROOF | LAVA_PROOF | UNACIDABLE | ACID_PROOF | FREEZE_PROOF
 	/// This tracks a new body that is created when a slime core is killed, and is used to safely trasnfer quirks and prefrences to this body and than summon it upon revival.
 	var/mob/living/carbon/human/new_body
+	/// This caches the mob's prefrences/quirks for revival.
+	var/datum/preferences/cached_prefs
 
 /obj/item/organ/brain/slime/Initialize(mapload, mob/living/carbon/organ_owner, list/examine_list)
 	. = ..()
@@ -270,34 +272,51 @@
 	if(core_ejected)
 		return
 	core_ejected = TRUE
-	victim.visible_message(span_warning("[victim]'s body completely dissolves, collapsing outwards!"), span_notice("Your body completely dissolves, collapsing outwards!"), span_notice("You hear liquid splattering."))
-	var/atom/death_loc = victim.drop_location()
 
 	// Create a new body in nullspace, and transfer their prefrences and quirks into it.
 	new_body = new(null)
-	victim.client?.prefs.safe_transfer_prefs_to(new_body)
+	//victim.client?.prefs.safe_transfer_prefs_to(new_body)
+	//victim.transfer_quirk_datums(new_body)
+
+	// Cache the victim's prefrences in the Brain/Core, and transfer them and their quirks to the new body.
+	if(victim.client?.prefs)
+		src.cached_prefs = victim.client.prefs
+	if(src.cached_prefs)
+		src.cached_prefs.safe_transfer_prefs_to(new_body)
 	victim.transfer_quirk_datums(new_body)
 
-	// Drop the Brain/Core, and implants to the floor.
-	for(var/obj/item/organ/organs in victim)
-		if(istype(organs, /obj/item/organ/brain) || istype(organs, /obj/item/implant))
-			Remove(organs, special = TRUE)
+	var/atom/death_loc = victim.drop_location()
+	if(!death_loc)
+		death_loc = get_turf(victim) // Fallback to avoid the Slime core showing up in Nullspace.
+
+	// Drop their equipment, Brain/Core, and implants to the floor.
+	victim.unequip_everything()
+	src.Remove(victim, special = TRUE) // Brain/Core
+	for(var/obj/item/implant/implants in victim) // Implants
+		implants.forceMove(death_loc)
+
+	// Move the Brain/Core, and implants to the death location.
 	if(death_loc)
 		forceMove(death_loc)
-		// Cleans up spilled organs - When a mob is attacked, it has a chance to spill all its organs on the ground upon death, for slime people we do not need their organs as they regain them when they get revived.
-		for(var/obj/item/organ/spilled_organ in death_loc)
-			if(istype(spilled_organ, /obj/item/organ/brain) || istype(spilled_organ, /obj/item/implant))
-				continue
-			else
-				qdel(spilled_organ)
+
+	// Cleans up spilled organs - When a mob is attacked, it has a chance to spill all its organs on the ground upon death, for slime people we do not need their organs as they regain them when they get revived.
+	for(var/obj/item/organ/spilled_organ in death_loc)
+		if(istype(spilled_organ, /obj/item/organ/brain) || istype(spilled_organ, /obj/item/implant))
+			continue
+		else
+			qdel(spilled_organ)
+
 	src.wash(CLEAN_WASH)
 	new death_melt_type(death_loc, victim.dir)
 
-	do_steam_effects(get_turf(victim))
-	playsound(victim, 'sound/effects/blob/blobattack.ogg', 80, TRUE)
+	do_steam_effects(death_loc)
+	playsound(death_loc, 'sound/effects/blob/blobattack.ogg', 80, TRUE)
 
 	if(gps_active) // adding the gps signal if they have activated the ability
 		AddComponent(/datum/component/gps, "[victim]'s Core")
+
+	// Message the victim and the surrounding area that they have died.
+	victim.visible_message(span_warning("[victim]'s body completely dissolves, collapsing outwards!"), span_notice("Your body completely dissolves, collapsing outwards!"), span_notice("You hear liquid splattering."))
 
 	qdel(victim) // Remove the body.
 	UnregisterSignal(victim, COMSIG_LIVING_DEATH)
@@ -350,6 +369,10 @@
 	if(gps_active) // making sure the gps signal is removed if it's active on revival
 		gps_active = FALSE
 		qdel(GetComponent(/datum/component/gps))
+
+	// Reapply prefrences from the Brain/Core in-case it failed in the Core Eject Proc.
+	if(src.cached_prefs)
+		src.cached_prefs.safe_transfer_prefs_to(new_body)
 
 	// Retreive the new body we created from nullspace.
 	new_body.forceMove(src.drop_location())