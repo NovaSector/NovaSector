dc99b322aa88fa2475a9b2df20df8c2f872b1e7b
Arceniu <129214736+Arceniu@users.noreply.github.com>
2026-08-19 21:58:38 +0300

    Update constants.ts

--- tgui/packages/tgui-say/constants.ts
+++ tgui/packages/tgui-say/constants.ts
@@ -36,7 +36,7 @@ export const RADIO_PREFIXES = {
   ':w ': 'Dyne',
   ':k ': 'Tark',
   ':q ': 'Csun',
-  ':, ': 'Guild',
-  ':. ': 'SolFed',
+  ':1 ': 'Guild',
+  ':2 ': 'SolFed',
   // NOVA EDIT ADDITION END
 } as const;
